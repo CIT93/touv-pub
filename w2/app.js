1c89479b6243c48e1dfc2cd27b980e4b38bc6884
Tou Vue <tvue686@gmail.com>
2026-08-17 17:51:39 -0700

    declaring variables

---
+++ w2/app.js
@@ -0,0 +1,16 @@
+console.log('Hello from app.js! Your JavaScript is connected and running!');
+// --- Part 1: Declaring Variables ---
+console.log('--- Part 1: Declaring Variables ---');
+
+// 'let' for values that can change
+// We can reassign 'let' variables
+let score = 50;
+console.log(`Initial score (let): ${score}`);
+score = 75;
+console.log(`Update score: ${score}`);
+
+// 'const' for values that should NOT change
+const GAME_TITLE = "My Awesome Game";
+console.log(`Game Title (const): ${GAME_TITLE}`);
+// GAME_TITLE = "Another Awesome Game";
+// Comment out above line error
