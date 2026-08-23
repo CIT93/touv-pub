cdb40707b8e37eb00b94c62752c3b442d0242b3f
Tou Vue <tvue686@gmail.com>
2026-08-22 21:30:08 -0700

    Setup 1 Complete

--- w2/app.js
+++ w2/app.js
@@ -1,16 +1,51 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
-// --- Part 1: Declaring Variables ---
-console.log('--- Part 1: Declaring Variables ---');
+// --- Part 1: Select HTML Elements ----
+// We use document.getElementById() to get a reference to an element by its unique ID.
+// We store these references in 'const' variables because the elements themselves won't change.
+const totalDisplayElement = document.getElementById("total-display");
+const addItemButton = document.getElementById("add-item-btn");
 
-// 'let' for values that can change
-// We can reassign 'let' variables
-let score = 50;
-console.log(`Initial score (let): ${score}`);
-score = 75;
-console.log(`Update score: ${score}`);
+//These variables will change as the user interacts with the page.
+let userName = 'Tou';
+let clickCount = 0;
 
-// 'const' for values that should NOT change
-const GAME_TITLE = "My Awesome Game";
-console.log(`Game Title (const): ${GAME_TITLE}`);
-// GAME_TITLE = "Another Awesome Game";
-// Comment out above line error
+// --- Part 2: Define a Function that Reacts to a Click---
+// A function is a block of code designed to perform a particular task.
+
+const handleButtonClick = function() {
+    // clickCount = clickCount + 1;
+    // Increase clickCount by 1 each time the button is clicked
+    clickCount += 1;
+    // Template strings (literal) to easily combine our variables and text into one message
+    let message = `Hello, ${userName}! You have clicked the button ${clickCount} time(s).`;
+    
+    // This is basic decision-making in JavaScript!
+    // Use a simple 'if' statement to make our page react differently based on clickCount.
+    if(clickCount >= 5) {
+        // We can even change the style of an HTML element directly with JavaScript!
+        // Change text color
+        message += ' Wow, you are a super clicker!';
+        messageDisplayElement.style.color = 'blue';
+    } else {
+        messageDisplayElement.style.color = '#333';
+    }
+    // Update the text content of our paragraph element on the page.
+    // This is how JavaScript makes changes visible on the web page!
+    messageDisplayElement.textContent = message;
+
+    console.log(`Button Clicked! Current click count: ${clickCount}`);
+};
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    // --- Part 3: Make the Button Clickable (Event Listener) ---
+    // This part ensures our JavaScript code runs only AFTER the HTML is fully loaded and parsed.    
+    // The 'DOMContentLoaded' event is perfect for this. It fires when the HTML document is ready.
+    
+    console.log('DOM fully loaded and parsed, App is ready for interaction')
+    // Attach an event listener to our 'updateButton'.
+    // When 'updateButton' receives a 'click' event, the 'handleButtonClick' function will execute.
+    updateButton.addEventListener('click', handleButtonClick);
+
+    messageDisplayElement.textContent = `Welcome, ${userName}! Click the button below to start counting`
+})
