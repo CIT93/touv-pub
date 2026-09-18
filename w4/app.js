271e7cb6cd978060123d5e076d99ceae056060d1
Tou Vue <tvue686@gmail.com>
2026-09-17 22:13:33 -0700

    Step 3 Complete

--- w4/app.js
+++ w4/app.js
@@ -1,21 +1,42 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
 
 import * as orderHandler from './order-handler.js';
+import * as priceCalculator from './price-calculator.js';
 
 // Reference to the order form.
 const orderForm = document.getElementById('order-form');
 
-// Referennce to the order summary
+// Reference to the order summary
 const orderSummary = document.getElementById('order-summary');
 
+// Stores all submitted orders.
+const orders = [];
+
 // Handles the form submission.
-const handleOrderSubmit = function(event) {
+const handleOrderSubmit = function (event) {
     event.preventDefault();
 
     const orderData = orderHandler.getOrderInputs();
-    
+
+    // Calculate the total price.
+    const calculatedPrice = priceCalculator.calculateTotal(orderData);
+
+    // Create a new order using the original data,
+    // calculated price, and a timestamp.
+    const newOrder = {
+        ...orderData,
+        ...calculatedPrice,
+        timestamp: new Date().toISOString()
+    };
+
+    // Store the new order
+    orders.push(newOrder);
+
+    // Verify the new orders array.
+    console.log(orders);
+
     let message = `Ordered ${orderData.qty} ${orderData.size} T-Shirts`;
-    
+
     if (orderData.giftWrap === true) {
         message += ' - gift wrapped';
     }
@@ -24,7 +45,7 @@ const handleOrderSubmit = function(event) {
 };
 
 // Initializes the application.
-const init = function() {
+const init = function () {
     console.log('App Initialized');
 
     orderForm.addEventListener('submit', handleOrderSubmit);
